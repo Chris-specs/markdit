0570f0f34ea9d33ba1bc9d4068ac5f0bc02a9184
Christian Sanchez <christian.s.molina@gmail.com>
2023-01-10 20:26:37 -0600

    Favicon and OG meta tags added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function LandingPage () {
             <h1 className='text-7xl font-extrabold text-transparent bg-clip-text bg-main-gradient'>
                 Markdit <span className='text-3xl font-medium'>v2</span>
             </h1>
-            <h2 className='w-full max-w-lg text-lg text-center py-4'>Make, preview and download README.MD and any markdown files fast.</h2>
+            <h2 className='w-full max-w-lg text-lg text-center py-4'>Type, preview and download README.MD and any markdown files fast.</h2>
             <Button href="/create">Start Now</Button>
         </section>
     )
