8b7f2357cfe2b2ea8ce6493434b3f7570e7d4b3e
Christian Sanchez <christian.s.molina@gmail.com>
2023-01-27 01:21:31 -0600

    Quick refactor with best practices added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-inter)']
-      },
-      backgroundImage: {
-        'main-gradient': 'linear-gradient(97.16deg, #FF4D4D 0%, #F9CB28 99.44%)'
-      },
-      colors: {
-        'brand-gray': '#AFAFAF'
-      }
+    content: [
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-inter)']
+            },
+            colors: {
+                'brand-gray': 'rgb(var(--brand-gray) / <alpha-value>)'
+            },
+            backgroundImage: {
+                'main-gradient': 'var(--main-gradient)'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 }
