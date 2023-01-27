aa993c6332d6a7cb54eff6ca7fa5edc68da1087f
Christian Sanchez <christian.s.molina@gmail.com>
2023-01-27 01:23:26 -0600

    Visual bugs fixed

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,7 +3,7 @@ import Editor from '@components/Editor'
 
 export default function CreatePage() {
     return (
-        <section>
+        <section className='h-[calc(100vh-72px)]'>
             <Editor />
         </section>
     )
